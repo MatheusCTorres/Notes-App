3e6eeeb9f3b8d3c31094197b348fb247498ff5de
Matheus Cardoso <46722962+MatheusCTorres@users.noreply.github.com>
2024-05-27 16:14:13 +0100

    Update NotesList.jsx

--- project/src/components/NotesList.jsx
+++ project/src/components/NotesList.jsx
@@ -1,11 +1,11 @@
 import AddNotes from "./AddNotes";
 import Notes from "./CardNotes";
 
-export default function NotesList({ notes, handleAddNote, handleDeleteNote, handleEditNote }) {
+export default function NotesList({ notes, handleAddNote, handleDeleteNote }) {
   return (
     <div className="notes-list">
       {notes.map((note) => (
-        <Notes id={note.id} text={note.text} date={note.date} key={note.id} handleDeleteNote={handleDeleteNote} handleEditNote={handleEditNote}/>
+        <Notes id={note.id} text={note.text} date={note.date} key={note.id} handleDeleteNote={handleDeleteNote} />
       ))}
       <AddNotes handleAddNote={handleAddNote} />
     </div>
